55514a527ab8ee4f05d4ce16f0222b67b1990e28
Hennan Lewis <hennan.lewis@gmail.com>
2023-05-16 23:49:25 -0300

    Mudança de código repetitivo para funções

--- blogger-auto-post-creator/main.py
+++ blogger-auto-post-creator/main.py
@@ -4,87 +4,63 @@ import pyautogui
 import pyperclip
 import os
 
-os.chdir (os.environ["PATH"])
+from functions import search_target
+from functions import target_to_click
 
 load_dotenv()
 
-blogLink = os.environ["BLOG_LINK"]
-screenPositionX = screenPositionY = 0
-postCounter = 1
+LOCAL_PATH = os.getcwd()
+os.chdir(LOCAL_PATH)
+
+BLOG_LINK = os.environ["BLOG_LINK"]
+screen_position_x = screen_position_y = 0
+post_number = 2
 # pyautogui.PAUSE = 0.2
 
-# 1 é a posição na barra de tarefas do ícone do navegador firefox
 pyautogui.hotkey("win","1")
 
-print("Aguarde, carregando firefox...")
-while True:
-	try:
-		pyautogui.locateOnScreen('images/firefox-window-icon.png')
-		break
-	except Exception as e:
-		print("Erro:", e)
+print("Aguarde, carregando Firefox...")
+search_target('images/firefox-window-icon.png')
 
 pyautogui.hotkey("ctrl","n")
 
 print("Aguarde, carregando nova aba...")
-while True:
-	try:
-		pyautogui.locateOnScreen('images/new-tab.png')
-		break
-	except Exception:
-		continue
+search_target('images/new-tab.png')
 
-pyautogui.typewrite(blogLink)
+pyautogui.typewrite(BLOG_LINK)
 pyautogui.hotkey('enter')
 
 print("Aguarde, página do blogger carregando...")
-while True:
-	try:
-		pyautogui.locateOnScreen('images/blog-loaded.png')
-		break
-	except Exception:
-		continue
+search_target('images/blog-loaded.png')
 
 print("Procurando botão para novo post...")
-while True:
-	try:
-		screenPositionX, screenPositionY = pyautogui.locateCenterOnScreen('images/new-post.png')
-		break
-	except Exception:
-		continue
+target_to_click('images/new-post.png')
 
-while postCounter <= 100:
+while post_number <= 100:
 	retryTime = 0
-	print("Criando novo post: Post", postCounter)
+	print("Criando novo post: Post", post_number)
 	while True:
 		try:
-			screenPositionX, screenPositionY = pyautogui.locateCenterOnScreen('images/new-post.png')
-			pyautogui.click(screenPositionX, screenPositionY)
-			pyautogui.locateOnScreen('images/return-button.png')
+			target_to_click('images/new-post.png')
+			search_target('images/return-button.png')
 			sleep(1)
-			screenPositionX, screenPositionY = pyautogui.locateCenterOnScreen('images/empty-title-post.png')
-			pyautogui.click(screenPositionX, screenPositionY)
+			target_to_click('images/empty-title-post.png')
 			break
 		except Exception:
 			print("Tentando novamente em", retryTime, "segundos")
 			sleep(retryTime)
 			retryTime += 2
 
-	if postCounter < 10:
-		postTitle = "Post " + str(postCounter).zfill(2)
+	if post_number < 10:
+		postTitle = "Post " + str(post_number).zfill(2)
 	else:
-		postTitle = "Post " + str(postCounter)
-	postCounter += 1
+		postTitle = "Post " + str(post_number)
+	post_number += 1
 	pyperclip.copy(postTitle)
 	pyautogui.hotkey("ctrl", "v")
 
-	while True:
-		sleep(1)
-		try:
-			pyautogui.locateOnScreen('images/title-updated.png')
-			break
-		except Exception:
-			continue
+	sleep(1)
+	search_target('images/title-updated.png')
 	
 	sleep(2.5)
 	pyautogui.hotkey("alt", "left")	
