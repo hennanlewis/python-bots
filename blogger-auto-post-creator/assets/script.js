af1c401f81c57753fac01ba1c86715f9c1f232e1
Hennan Lewis <hennan.lewis@gmail.com>
2023-06-20 01:50:04 -0300

    Rearranging JavaScript code functions

--- blogger-auto-post-creator/assets/script.js
+++ blogger-auto-post-creator/assets/script.js
@@ -1,75 +1,112 @@
-function addLinkForm(position) {
-	const formulario = document.createElement("form")
+const createUrlLabel = (position) => {
+	const label = document.createElement("label")
+	const span = document.createElement("span")
+	span.textContent = "Link url"
+	label.appendChild(span)
 
-	const label1 = document.createElement("label")
-	const span1 = document.createElement("span")
-	span1.textContent = "Link url"
-	const input1 = document.createElement("input")
-	input1.type = "url"
-	input1.name = `link-url${position}`
-	label1.appendChild(span1)
-	label1.appendChild(input1)
+	const input = document.createElement("input")
+	input.type = "url"
+	input.id = `link-url${position}`
+	input.name = `link-url${position}`
+	label.appendChild(input)
 
-	const label2 = document.createElement("label")
-	const span2 = document.createElement("span")
-	span2.textContent = "Initial post number"
-	const input2 = document.createElement("input")
-	input2.type = "number"
-	input2.name = `initial_post${position}`
-	input2.min = "1"
-	input2.value = "1"
-	label2.appendChild(span2)
-	label2.appendChild(input2)
-
-	const label3 = document.createElement("label")
-	const span3 = document.createElement("span")
-	span2.textContent = "Quantity for insertion"
-	const input3Range = document.createElement("input")
-	input3Range.type = "range"
-	input3Range.name = `quantity_range${position}`
-	input3Range.min = "1"
-	input3Range.max = "100"
-	input3Range.value = "1"
-	const input3Number = document.createElement("input")
-	input3Number.type = "number"
-	input3Number.name = `quantity_number${position}`
-	input3Number.min = "1"
-	input3Number.max = "100"
-	input3Number.value = "1"
-	label3.appendChild(span3)
-	label3.appendChild(input3Range)
-	label3.appendChild(input3Number)
-
-	const button = document.createElement("button")
-	button.type = "button"
-	button.textContent = "Remove item"
-
-	formulario.appendChild(label1)
-	formulario.appendChild(label2)
-	formulario.appendChild(label3)
-	formulario.appendChild(button)
-
-
-	const container = document.querySelector("main")
-	container.appendChild(formulario)
+	return label
 }
 
-function validateURL(url) {
+const createInitialPostNumberLabel = (position) => {
+	const label = document.createElement("label")
+	const span = document.createElement("span")
+	span.textContent = "Initial post number"
+	label.appendChild(span)
+
+	const input = document.createElement("input")
+	input.type = "number"
+	input.id = `initial-post${position}`
+	input.name = `initial-post${position}`
+	input.min = "1"
+	input.value = "1"
+	label.appendChild(input)
+
+	return label
+}
+
+const createPostQuantity = (position) => {
+	const label = document.createElement("label")
+	const span = document.createElement("span")
+	span.textContent = "Quantity for insertion: "
+	const quantity = document.createElement("b")
+	quantity.id = `quantity-value${position}`
+	quantity.textContent = "100"
+	span.appendChild(quantity)
+	label.appendChild(span)
+
+	const inputRange = document.createElement("input")
+	inputRange.type = "range"
+	inputRange.id = `post-quantity${position}`
+	inputRange.name = `post-quantity${position}`
+	inputRange.min = "1"
+	inputRange.max = "100"
+	inputRange.value = "100"
+	inputRange.addEventListener("change", () => {
+		const quantityValue = document.querySelector(`#quantity-value${position}`)
+		quantityValue.textContent = Math.min(Math.max(inputRange.value, 0), 100)
+	})
+	label.appendChild(inputRange)
+
+	return label
+}
+
+const createRemoveItemButton = (position) => {
+	const button = document.createElement("button")
+	button.type = "button"
+	button.textContent = "Remove link"
+	button.addEventListener("click", () => {
+		const formToRemove = document.querySelector(`form#position${position}`)
+		formToRemove.classList.add("height0")
+		optionsArray = optionsArray.filter(item => item.position != position)
+		setInterval(() => formToRemove.remove(), 1000)
+	})
+
+	return button
+}
+
+const addLinkForm = (position) => {
+	const form = document.createElement("form")
+	form.id = `position${position}`
+
+	const urlLabel = createUrlLabel(position)
+	form.appendChild(urlLabel)
+
+	const initialPostNumberLabel = createInitialPostNumberLabel(position)
+	form.appendChild(initialPostNumberLabel)
+
+	const postQuantityLabel = createPostQuantity(position)
+	form.appendChild(postQuantityLabel)
+
+	const removeItemButton = createRemoveItemButton(position)
+	form.appendChild(removeItemButton)
+
+	const container = document.querySelector("main")
+	container.appendChild(form)
+}
+
+const isValidURL = (url) => {
 	const patronURL1 = /^https?:\/\/www\.blogger\.com\/blog\/posts\/\d+$/
 	const patronURL2 = /^https?:\/\/www\.blogger\.com\/u\/\d+\/blog\/posts\/\d+$/
 	return patronURL1.test(url) || patronURL2.test(url)
 }
 
-const optionsArray = []
+let optionsArray = []
+let optionPosition = 0
 
-const addLink = document.querySelector("#add-item")
-
-addLink.addEventListener("click", () => {
-	addLinkForm(optionsArray.length + 1)
+const addLinkButton = document.querySelector("#add-item")
+addLinkButton.addEventListener("click", () => {
+	optionPosition += 1
+	addLinkForm(optionPosition)
 	optionsArray.push({
 		url_link: "",
 		initial_post: 0,
-		final_post: 1
+		post_quantity: 1,
+		position: optionPosition
 	})
-	console.log(optionsArray)
 })
